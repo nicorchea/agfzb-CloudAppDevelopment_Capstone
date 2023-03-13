5175aa7a3497cbbad7c15447659cc8b2a0d89f9f
Nikito <nikito@pop-os.localdomain>
2023-03-13 11:26:44 -0300

    added car delaer y maker

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,20 @@ from django.contrib import admin
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+from .models import CarMake, CarModel
+
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'make', 'car_type', 'year', 'dealer_id')
+    list_filter = ('make', 'car_type', 'year')
+    search_fields = ('name', 'make__name')
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
